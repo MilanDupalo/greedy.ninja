52098a8ccb4b473b9af2fbe3a5a626c5d729fe58
MilanDupalo <milandupalodupi@gmail.com>
2021-12-26 23:53:05 +0100

    brand section all done

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import "./brand.css";
 
+import { google, slack, atlassian, dropbox, shopify } from "./import.js";
+
 export const Brand = () => {
-  return <div>brand</div>;
+  return (
+    <section className="brand section__padding">
+      <figure className="brand-image-box">
+        <img className="brand-image" src={google} alt="google" />
+      </figure>
+      <figure className="brand-image-box">
+        <img className="brand-image" src={slack} alt="slack" />
+      </figure>
+      <figure className="brand-image-box">
+        <img className="brand-image" src={atlassian} alt="atlassian" />
+      </figure>
+      <figure className="brand-image-box">
+        <img className="brand-image" src={dropbox} alt="dropbox" />
+      </figure>
+      <figure className="brand-image-box">
+        <img className="brand-image" src={shopify} alt="shopify" />
+      </figure>
+    </section>
+  );
 };
 
 export default Brand;
