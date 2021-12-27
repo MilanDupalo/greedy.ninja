c1c62139a8986830c542fdf1f7ecab9a2bb6283a
MilanDupalo <milandupalodupi@gmail.com>
2021-12-27 12:48:45 +0100

    possibility section component(html)

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 import "./possibility.css";
+import possibility from "../../assets/possibility.png";
 
 export const Possibility = () => {
-  return <div>Possibility</div>;
+  return (
+    <section className="possibility section__padding" id="possibility">
+      <figure className="possibility-image-box">
+        <img src={possibility} alt="possibilityImage" />
+      </figure>
+      <section className="possibility-content">
+        <h4 className="possibility-content-subtitle">
+          Request Early Access to Get Started{" "}
+        </h4>
+        <h3 className="gradient__text possibility-content-title">
+          Lorem ipsum dolor sit amet consectetur.
+        </h3>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <a className="possibility-content-link" href="#">
+          Request Early Access to Get Started
+        </a>
+      </section>
+    </section>
+  );
 };
 
 export default Possibility;
