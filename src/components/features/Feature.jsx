fdc7b51bb383566523a3942e6bc633b91c9a9844
MilanDupalo <milandupalodupi@gmail.com>
2021-12-27 11:14:17 +0100

    about section done(css and media query)

--- src/components/features/Feature.jsx
+++ src/components/features/Feature.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./feature.css";
 
-export const Feature = () => {
-  return <div>Featurdsadsadasdasdasdasdassadase</div>;
+export const Feature = ({ title, text }) => {
+  return (
+    <article className="features-container__feature">
+      <h3 className="features-container__feature-title">{title}</h3>
+      <p className="features-container__feature-text">{text}</p>
+    </article>
+  );
 };
 
 export default Feature;
--- src/containers/whatGreedyNinja/WhatGreedyNinja.jsx
+++ src/containers/whatGreedyNinja/WhatGreedyNinja.jsx
@@ -4,20 +4,36 @@ import "./whatGreedyNinja.css";
 
 export const WhatGreedyNinja = () => {
   return (
-    <article className="about section__padding" id="whatGreedyNinja">
-      <section className="about-features">
-        <Feature />
+    <article className="about section__margin" id="whatGreedyNinja">
+      <section className="about-feature">
+        <Feature
+          title="About us"
+          text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."
+        />
       </section>
       <section className="about-heading">
-        <h2 className="gradient__text ">
+        <h2 className="gradient__text about-heading-title">
           Lorem ipsum dolor sit amet consectetur
         </h2>
-        <a href="#"> Explore The Library </a>
+        <a className="about-heading-link" href="#">
+          {" "}
+          Explore The Library{" "}
+        </a>
       </section>
+
       <section className="about-container">
-        <Feature />
-        <Feature />
-        <Feature />
+        <Feature
+          title="Chatbots"
+          text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. "
+        />
+        <Feature
+          title="Knowledgebase"
+          text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+        />
+        <Feature
+          title="Education"
+          text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+        />
       </section>
     </article>
   );
