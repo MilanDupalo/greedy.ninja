232ce230b262a0360bed217b34c25866fed8b13d
MilanDupalo <milandupalodupi@gmail.com>
2021-12-27 13:27:38 +0100

    cta components (html)

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -2,7 +2,21 @@ import React from "react";
 import "./cta.css";
 
 export const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <article className="cta">
+      <section className="cta-contnet">
+        <h4 className="cta-content-subtitle">
+          Request Early Access to Get Started
+        </h4>
+        <h3 className="cta-content-title">
+          Register today & start exploring the endless possiblities.
+        </h3>
+      </section>
+      <section className="cta-button-box">
+        <button className="btn cta-button"> Get Started</button>
+      </section>
+    </article>
+  );
 };
 
 export default CTA;
